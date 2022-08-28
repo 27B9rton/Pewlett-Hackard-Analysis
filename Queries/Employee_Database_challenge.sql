7435553b1a43b717caa97c80d11eed09c01b11ec
Sean Barton <seanbarton34@gmail.com>
2022-08-28 14:02:42 -0600

    Challenge Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -31,3 +31,18 @@ FROM unique_titles
 GROUP BY title
 ORDER BY (count) DESC;
 SELECT * FROM retiring_titles;
+
+SELECT DISTINCT ON (emp_no) e.emp_no, e.first_name, e.last_name,
+e.birth_date, de.from_date, de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (ti.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY (e.emp_no) ASC;
+
+SELECT * FROM mentorship_eligibility;
